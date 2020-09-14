5858265e66ae7d7f5a3e73eae33a87b0dde2fe90
Evgenii Sopov <mrseakg@gmail.com>
2020-09-15 05:00:44 +0700

    Init empty ios app

---
+++ ios/wsjcppobjtreetest/wsjcppobjtreetest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wsjcppobjtreetest
+//
+//  Created by sea-kg on 15.09.2020.
+//  Copyright © 2020 sea-kg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
